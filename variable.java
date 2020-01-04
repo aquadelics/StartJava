93355fbc39984b5a11f560771826c915a3fe3f80
Kirill Ivanov <aquadelics@gmail.com>
2020-01-04 22:01:38 +0300

    Homework #1 Variable

---
+++ variable.java
@@ -0,0 +1,21 @@
+public class Variable { 
+    public static void main(String[] args) {
+       
+       byte a = 4;
+       short b = 4095;
+       int c = 128;
+       long d = 553535663;
+       float e = 1.5553f;
+       double f = 5.35363634634888d;
+       char g = 'Z';
+       boolean  h = false;
+System.out.println("Count of cores: " +  a);
+System.out.println("RAM: " + b);
+System.out.println("Primary hard drive, GB: " + c);
+System.out.println("Count of music files: " + d);
+System.out.println("Some float expression :) " + e);
+System.out.println("Some double expression: " + f);
+System.out.println("Last virtual drive letter: " + g);
+System.out.println("It is laptop? " + h);
+    }
+}
