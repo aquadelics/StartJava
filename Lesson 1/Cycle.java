1d750f181b0904011e8a1cc809951450463c0a3d
Kirill Ivanov <aquadelics@gmail.com>
2020-02-12 23:53:01 +0300

    Fixed Cycle

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,23 +1,24 @@
 public class Cycle {
 	public static void main(String[] args) {
-		for (int i = 0; i <= 20; i++ ) {
+		for(int i = 0; i <= 20; i++ ) {
 			System.out.println(i);
 		}
 
 		int i = 6;
-		while (i >= -6) {
+		while(i >= -6) {
 			System.out.print(i + " ");
 			i -= 2;
 		}
-
 		System.out.println();
 
-		int i = 2;
-		int result = 1;
+		int p = 10;
+		int sum = 0;
 		do {
-			result = result + i;
-			i++;
-		} while(result > 9 && result < 21);
-		System.out.print (result);
-		}
+			if(p % 2 != 0) {
+				sum = sum + p;
+			}
+			p++;
+		} while(p <= 20);
+		System.out.println(sum);
 	}
+}
